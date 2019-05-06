f3b66e0c00db65cce30d014221fc7515c84dcd12
blenz <bbluegga@yahoo.com>
2019-05-05 21:15:24 -0700

    add personlist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PersonList from './PersonList';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <PersonList />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/PersonList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectPerson } from '../actions';
+
+class PersonList extends Component {
+  renderPeople() {
+    return this.props.people.map(person => {
+      return (
+        <li className="list-group-item" key={person.id}>
+          {person.name}
+          <button
+            className="btn btn-sm btn-info float-right"
+            onClick={() => this.props.selectPerson(person)}
+          >
+            Select
+          </button>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="col-4">
+        <div className="text-center">Person List</div>
+        <div>
+          <ul className="list-group">{this.renderPeople()}</ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { people: state.people };
+};
+
+export default connect(
+  mapStateToProps,
+  { selectPerson }
+)(PersonList);
