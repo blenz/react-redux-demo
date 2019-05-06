8e44c9f8189f0ed820984bd921b39d3f54014910
blenz <bbluegga@yahoo.com>
2019-05-05 21:16:00 -0700

    add mock data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,24 +2,24 @@ import { combineReducers } from 'redux';
 
 const personReducer = () => {
   return [
-    { name: 'Jim', Age: 10, Address: '1000 Test St' },
-    { name: 'Bob', Age: 20, Address: '2000 Test St' },
-    { name: 'Hope', Age: 30, Address: '3000 Test St' },
-    { name: 'Ken', Age: 40, Address: '4000 Test St' },
-    { name: 'Stacey', Age: 40, Address: '5000 Test St' }
+    { id: 1, name: 'Jim', Age: 10, Address: '1000 Test St' },
+    { id: 2, name: 'Bob', Age: 20, Address: '2000 Test St' },
+    { id: 3, name: 'Hope', Age: 30, Address: '3000 Test St' },
+    { id: 4, name: 'Ken', Age: 40, Address: '4000 Test St' },
+    { id: 5, name: 'Stacey', Age: 40, Address: '5000 Test St' }
   ];
 };
 
-const selectedPersonReducer = (person, action) => {
+const selectedPersonReducer = (selectedPerson = null, action) => {
   switch (action.type) {
     case 'SELECT_PERSON':
       return action.payload;
     default:
-      return person;
+      return selectedPerson;
   }
 };
 
 export default combineReducers({
-  person: personReducer,
+  people: personReducer,
   selectedPerson: selectedPersonReducer
 });
